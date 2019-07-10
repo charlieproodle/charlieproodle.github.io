441b3ea3c527757e69d1955a9baa9c5122bf37c3
charlieproodle <charlie@proodlesolutions.com>
2019-07-11 00:31:52 +0100

    Code removal

--- static/media/Text/info.js
+++
@@ -1,9 +0,0 @@
-export const proodle = `React and React Native developer for Proodle Solutions, a startup located in Nottingham.
-Working with a Team on developing a University branded application for both mobile and web platform.`;
-
-export const gan_project = `Current Masters project investigating GANs (Generative Adversarial Networks), a type
-of Neural Network architecture, to generate convincing novel content such as Music, Images, and Text`;
-
-export const signup_project = `Personal project to speed up startup development of signup/login pages using
-Django and React`;
-
