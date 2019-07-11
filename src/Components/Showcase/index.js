8ba873b81cbed5d39c84e5f93addc6c52ae22bee
charlieproodle <charlie@proodlesolutions.com>
2019-07-11 01:26:59 +0100

    Update to styling

---
+++ src/Components/Showcase/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { withStyles } from "@material-ui/styles";
+import styles from "./styles";
+import { IoIosRocket } from "react-icons/io";
+
+const Showcase = props => {
+  const { classes, icon, text, link, stack, color } = props;
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.icon}>
+        <img src={icon} alt={"Not found"} className={classes.iconImg} />
+      </div>
+      <div className={classes.text}>{text}</div>
+      <div className={classes.stack}>{stack}</div>
+      <div className={classes.link} onClick={() => window.location = link}>
+          See project <IoIosRocket style={{color: color, marginLeft: "5%"}}/>
+        </div>
+    </div>
+  );
+};
+
+export default withStyles(styles)(Showcase);
---
+++ src/Components/Showcase/styles.js
@@ -0,0 +1,38 @@
+const styles = {
+  container: {
+    margin: "2.5%",
+    border: "solid 2px lightgray",
+    paddingTop: "20px",
+    borderRadius: "5px",
+    flex: 1,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+  },
+  icon: {
+    textAlign: "center",
+    marginBottom: "20px",
+    padding: "20px",
+    borderBottom: "solid 1px lightgray"
+  },
+  iconImg: {
+    height: "50px"
+  },
+  text: {
+    marginBottom: "20px",
+    padding: "0 20px",
+    fontFamily: "Roboto"
+  },
+  stack: {
+    paddingLeft: "5%"
+  },
+  link: {
+    padding: "20px 20px",
+    cursor: "pointer",
+    "&:hover": {
+      backgroundColor: "lightgray"
+    }
+  }
+};
+
+export default styles;
